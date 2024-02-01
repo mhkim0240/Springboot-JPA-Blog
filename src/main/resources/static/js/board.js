f337ee550cf61893e677ddf5361e8e3e8893b4ad
Leo Kim <xowldidi@naver.com>
2024-02-01 22:10:17 +0900

    53강 글쓰기 완료.

---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,34 @@
+let index = {
+
+	//jQuery 
+	init: function() {
+		$("#btn-save").on("click", () => {
+			this.save();
+		});
+	},
+
+	save: function() {
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+
+		console.log(data);
+
+		$.ajax({
+			type: "POST",
+			url: "/api/board",
+			data: JSON.stringify(data), 
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("글쓰기가 완료되었습니다." + resp);
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+
+		});
+	}
+}
+
+index.init();
